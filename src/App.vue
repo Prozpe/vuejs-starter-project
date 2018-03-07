fc91aecf426ff90a6c08af948c907f4619dcaaf1
ksidedev <snoobeck75@gmail.com>
2018-03-08 12:34:19 +1300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ComponentOne />
+    <ComponentTwo />
+  </div>
+</template>
+
+<script>
+import ComponentOne from './components/ComponentOne';
+import ComponentTwo from './components/ComponentTwo';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      keyword: '',
+      onOff: true,
+    };
+  },
+  components: {
+    ComponentOne,
+    ComponentTwo,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+.box {
+  width: 100px;
+  height: 100px;
+  border: 1px solid rgba(0, 0, 0, 0.12);
+  margin: 10px auto 0;
+}
+</style>
---
+++ src/components/ComponentTwo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="ComponentTwo">
+    <input placeholder="type colour" type="text" v-model="keyword" />
+    <div class="box" v-bind:style="{backgroundColor: keyword}"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentTwo',
+  data() {
+    return {
+      keyword: '',
+      onOff: true,
+    };
+  },
+  methods: {
+    toggleOnOff() {
+      this.onOff = !this.onOff;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#ComponentTwo {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+.box {
+  width: 100px;
+  height: 100px;
+  border: 1px solid rgba(0, 0, 0, 0.12);
+  margin: 10px auto 0;
+}
+</style>
