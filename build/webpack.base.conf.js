5c2a34c01636c96f0877a760fbf2458e9a6ae71a
ksidedev <snoobeck75@gmail.com>
2018-03-08 14:43:02 +1300

    Tidy folder structure

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -75,19 +75,8 @@ module.exports = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
-    ],
-    loaders: [
-      {
-        test: /\.vue$/,
-        loader: 'vue'
-      }, 
-      {
-          test: /\.s[a|c]ss$/,
-          loader: 'style!css!sass'
-      }
     ]
   },
-  
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
